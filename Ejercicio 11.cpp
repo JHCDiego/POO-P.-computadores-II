712d4811962ae03197bef02964f071b24e2eda97
JHCDiego <87152346+JHCDiego@users.noreply.github.com>
2021-10-03 16:02:32 -0500

    Add files via upload
    Dudas con los ejercicios 8 y 9. Son similares

---
+++ Ejercicio 11.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+class cifras{
+private:
+    int numero; int* digitos;
+    int contador = 0;
+public:
+    cifras(int n){
+    numero = n;
+    }
+
+    void contarcifras(){
+        int tmp = numero;
+            while(tmp!=0){
+                    contador++;
+                    tmp/=10;
+                }
+    }
+
+   void guardar_digitos(){
+            digitos = new int[contador];
+            for(int i=0;i<contador;i++){
+                digitos[i] = numero%10;
+                numero/=10;
+            }
+    }
+    int suma_digitos(){
+        int suma = 0;
+        for(int i=0;i<contador;i++){
+            suma+=digitos[i];
+        }
+        return suma;
+    }
+    void multiplo(){
+        int x = suma_digitos();
+        for(int i=0;i<contador;i++){
+        if(digitos[i]==0){
+                cout<<x<<" es multiplo de "<<digitos[i]<<endl;
+           }
+        else{
+            if(x%digitos[i]==0){
+                cout<<x<<" es multiplo de "<<digitos[i]<<endl;
+            }
+            else{
+                cout<<x<<" no es multiplo de "<<digitos[i]<<endl;
+            }
+        }
+
+    }
+}
+};
+
+int main(){
+    int z;
+    cout<<"Ingrese un numero: "; cin>>z;
+    cifras n(z);
+    n.contarcifras();
+    n.guardar_digitos();
+    cout<<"La suma de los digitos de "<<z<<" es: "<<n.suma_digitos()<<endl;
+    n.multiplo();
+    return 0;
+}
---
+++ Ejercicio 8 y 9.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+class Operacion{
+
+	private:
+		int n; double* numeros;
+	public:
+	    double suma = 0;
+		Operacion(int m) {
+            n = m;
+		};
+        void datos(){
+        numeros = new double[n];
+            for(int i=0;i<n;i++){
+                cout<<"numero_"<<i+1<<" = ";
+                cin>>numeros[i];
+            }
+        }
+
+		void sumatoria() {
+			for (int i=0;i<n;i++){
+                suma += numeros[i];
+			}
+			cout<<"Suma = "<<suma<<endl;
+		}
+
+		void promedio(){
+			cout<<"Promedio = "<<suma/n<<endl;
+		}
+};
+
+int main() {
+    int z;
+	cout<<"Calcular la suma y el promedio de n numeros: "<<endl;
+	cout<<"n = "; cin>>z;
+
+	Operacion ob1(z);
+	ob1.datos();
+	ob1.sumatoria();
+	ob1.promedio();
+
+	return 0;
+}
